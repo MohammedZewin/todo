82274ae1695859f429ea93760516133270e2054d
Zewin <0558568986m@gmail.com>
2022-12-20 00:47:14 +0200

    Structure

---
+++ lib/layout/home_layout/home_layout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeLayout extends StatelessWidget {
+  const HomeLayout({Key? key}) : super(key: key);
+  static const String routName = "HomeLayout";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:todofinal/styles/myTheme.dart';
+
+import 'layout/home_layout/home_layout.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: HomeLayout.routName,
+      routes: {
+        HomeLayout.routName: (context) => HomeLayout(),
+      },
+      theme: MyThemeData.lightTheme,
+      darkTheme: MyThemeData.darkTheme,
+      themeMode: ThemeMode.light,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
